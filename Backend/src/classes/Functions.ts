0c6481c3a4cc78fe865e2c8ced6be1ab7e357852
Mauritz N <mauritz@europe.com>
2019-05-01 20:11:17 +0300

    Add hash method

--- Backend/src/classes/Functions.ts
+++ Backend/src/classes/Functions.ts
@@ -1,4 +1,7 @@
+import * as crypto from "crypto";
 import { ExpressResponse } from "../config/Interfaces";
+
+const supportedHashAlgos: string[] = ["RSA-MD4", "RSA-MD5", "RSA-MDC2", "RSA-RIPEMD160", "RSA-SHA1", "RSA-SHA1-2", "RSA-SHA224", "RSA-SHA256", "RSA-SHA384", "RSA-SHA512", "blake2b512", "blake2s256", "md4", "md4WithRSAEncryption", "md5", "md5-sha1", "md5WithRSAEncryption", "mdc2", "mdc2WithRSA", "ripemd", "ripemd160", "ripemd160WithRSA", "rmd160", "sha1", "sha1WithRSAEncryption", "sha224", "sha224WithRSAEncryption", "sha256", "sha256WithRSAEncryption", "sha384", "sha384WithRSAEncryption", "sha512", "sha512WithRSAEncryption", "ssl3-md5", "ssl3-sha1", "whirlpool"];
 
 export class Functions {
   static logCallback(err: any) {
@@ -66,5 +69,15 @@ export class Functions {
     } else {
       return false;
     }
+  }
+
+
+  static hash(toHash: string, hashAlgo: string = "sha256") {
+    if((supportedHashAlgos).indexOf(hashAlgo) < 0) {
+      hashAlgo = "sha256";
+    }
+
+    const cryptoHash = crypto.createHash(hashAlgo);
+    return cryptoHash.update(toHash, "utf8").digest("hex");
   }
 }
